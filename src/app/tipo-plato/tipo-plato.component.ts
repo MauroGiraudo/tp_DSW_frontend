347da2fa09b57ee48685b0cd0b9c48abd66602b8
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-08-27 22:22:59 -0300

    Algunos componentes generados

---
+++ src/app/tipo-plato/tipo-plato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-plato',
+  standalone: true,
+  imports: [],
+  templateUrl: './tipo-plato.component.html',
+  styleUrl: './tipo-plato.component.scss'
+})
+export class TipoPlatoComponent {
+
+}
