ac779431c9ba45bd66977072ab500784f533c79c
unknown <juanjosepastorino@gmail.com>
2025-02-16 16:22:24 -0300

    Cambio de url reseña

--- src/app/service/resena.service.ts
+++ src/app/service/resena.service.ts
@@ -2,29 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResponseResenas } from '../models/mesa.models.js';
+import { UsuarioService } from './usuario.service.js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResenaService {
-  private apiUrl = 'http://localhost:3000/api/pedidos';
+  private apiUrl = 'http://localhost:3000/api/clientes';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private usuarioService: UsuarioService,
+  ) {}
 
   obtenerResenas(): Observable<ResponseResenas> {
     return this.http.get<ResponseResenas>('http://localhost:3000/api/resenas');
   }
 
   crearResena(nroPed: number, resena: { cuerpo: string; puntaje: number }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/${nroPed}/resena`, resena);
+    const clienteId = this.usuarioService.obtenerUsuarioActual().id;
+    return this.http.post(`${this.apiUrl}/${clienteId}/pedidos/${nroPed}/resena`, resena);
   }
 
   modificarResena(nroPed: number, resena: { cuerpo: string; puntaje: number }): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${nroPed}/resena`, resena);
+    const clienteId = this.usuarioService.obtenerUsuarioActual().id;
+    return this.http.put(`${this.apiUrl}/${clienteId}/pedidos/${nroPed}/resena`, resena);
   }
 
   eliminarResena(nroPed: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${nroPed}/resena`);
+    const clienteId = this.usuarioService.obtenerUsuarioActual().id;
+    return this.http.delete(`${this.apiUrl}/${clienteId}/pedidos/${nroPed}/resena`);
   }
 }
 
